46970247c68470d9f428d9a66b305b4b89e6a78c
WanruXX <wanru.xia1@gmail.com>
2024-04-06 02:55:23 +0200

    initialize an environment with a 2d canvas setup

---
+++ minesweeper/src/main.rs
@@ -0,0 +1,33 @@
+use bevy::prelude::*;
+use bevy::window::{Window, WindowResolution, WindowPlugin};
+
+#[cfg(debug_assertions)]
+use bevy_inspector_egui::quick::WorldInspectorPlugin;
+use bevy::input::common_conditions;
+
+
+
+fn main() {
+    let mut app = App::new();
+    // Window setup
+    app.add_plugins(DefaultPlugins.set(WindowPlugin {
+        primary_window: Some(Window {
+            resolution: WindowResolution::new(700., 800.),
+          title: "Mine Sweeper".to_string(),
+          ..default()
+        }),
+        ..default()
+      }));
+
+    #[cfg(debug_assertions)]
+    app.add_plugins(WorldInspectorPlugin::default().run_if(common_conditions::input_toggle_active(true, KeyCode::Escape)));
+
+    app.add_systems(Startup, setup_camera);
+    app.run();
+}
+
+fn setup_camera(mut commands: Commands) {
+    commands.spawn(
+        Camera2dBundle::default()
+    );
+}
