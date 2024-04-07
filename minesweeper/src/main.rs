c6432a3032603fc09ef231e54d28eb8bcfd95781
WanruXX <wanru.xia1@gmail.com>
2024-04-07 03:54:22 +0200

    initilize board map including setting up bombs and calculating  state for each tile

--- minesweeper/src/main.rs
+++ minesweeper/src/main.rs
@@ -1,12 +1,11 @@
 use bevy::prelude::*;
-use bevy::window::{Window, WindowResolution, WindowPlugin};
+use bevy::window::{Window, WindowPlugin, WindowResolution};
+use board_plugin::BoardPlugin;
 
-#[cfg(feature = "inspect")]
-use bevy_inspector_egui::quick::WorldInspectorPlugin;
 #[cfg(feature = "inspect")]
 use bevy::input::common_conditions;
-
-
+#[cfg(feature = "inspect")]
+use bevy_inspector_egui::quick::WorldInspectorPlugin;
 
 fn main() {
     let mut app = App::new();
@@ -14,21 +13,22 @@ fn main() {
     app.add_plugins(DefaultPlugins.set(WindowPlugin {
         primary_window: Some(Window {
             resolution: WindowResolution::new(700., 800.),
-          title: "Mine Sweeper".to_string(),
-          ..default()
+            title: "Mine Sweeper".to_string(),
+            ..default()
         }),
         ..default()
-      }));
+    }))
+    .add_plugins(BoardPlugin);
 
-      #[cfg(feature = "inspect")]
-    app.add_plugins(WorldInspectorPlugin::default().run_if(common_conditions::input_toggle_active(true, KeyCode::Escape)));
+    #[cfg(feature = "inspect")]
+    app.add_plugins(WorldInspectorPlugin::default().run_if(
+        common_conditions::input_toggle_active(true, KeyCode::Escape),
+    ));
 
     app.add_systems(Startup, setup_camera);
     app.run();
 }
 
 fn setup_camera(mut commands: Commands) {
-    commands.spawn(
-        Camera2dBundle::default()
-    );
+    commands.spawn(Camera2dBundle::default());
 }
